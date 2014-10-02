e6843c6b3fab22ae1bece00a1526b128e1272832
Kinan Alhoch <kalhoch@gmail.com>
2014-10-02 10:43:11 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Valingo::Application.config.secret_token = '0eb5b1d95d64648a1aa0434ca7ce8b68fb8f0aad1939f692045169936df8a5e0f5bca4546ce48b6d92fee32d21d6a1e065ad1092c79ae3db2ca552e39ba337e7'
